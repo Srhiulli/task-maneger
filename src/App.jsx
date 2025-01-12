8e256290248f1b386c6fa713f7783ffce7d7a363
Srhiulli <srhiulli@gmail.com>
2025-01-12 20:33:06 -0300

    feat: add Button component and initial task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar"
+import TasksPage from "./components/Tasks"
 
 function App() {
   return (
-    <>
+    <div className="flex gap-9">
       <Sidebar />
-    </>
+      <TasksPage />
+    </div>
   )
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,28 @@
+import AddIcon from "../assets/icons/add.svg?react"
+import SendTotrash from "../assets/icons/trash.svg?react"
+import Button from "./Button"
+
 function TasksPage() {
   return (
-    <div className="flex">
-      <h1 className="text-red-500">LALALA LE</h1>
+    <div className="w-full px-8 py-16">
+      <div className="flex w-full justify-between">
+        <div>
+          <span className="text-xs font-semibold text-[#00ADB5]">
+            Minhas tarefas
+          </span>
+          <h2 className="text-red-500">Minhas tarefas</h2>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button variant="secundary">
+            Limpar tarefas
+            <SendTotrash />
+          </Button>
+          <Button variant="primary">
+            Nova tarefa
+            <AddIcon />
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
