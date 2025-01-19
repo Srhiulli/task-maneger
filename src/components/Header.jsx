d0659432cf09c9bd1c12c7b199224ccce124e009
Srhiulli <srhiulli@gmail.com>
2025-01-19 18:14:59 -0300

    chore: add props and propTypes to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
-function Header() {
-  return <div className="flex w-full justify-between">Header legal</div>
+import PropTypes from "prop-types"
+
+function Header(props) {
+  return <div className="flex w-full justify-between">{props.children}</div>
+}
+
+Header.prototype = {
+  children: PropTypes.node.isRequired,
 }
 
 export default Header
