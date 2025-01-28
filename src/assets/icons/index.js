dc63a9734c32478a0c91c182a8e4af48810721ff
Srhiulli <srhiulli@gmail.com>
2025-01-28 20:36:20 -0300

    feat: dashboard to homePage

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,11 +4,13 @@ import CheckIcon from "./check.svg?react"
 import ChevronRightIcon from "./chevron-right.svg?react"
 import CludSunIcon from "./cloud-sun.svg?react"
 import DetailsIcon from "./details.svg?react"
+import GlassWaterIcon from "./glass-water.svg?react"
 import HomeIcon from "./home.svg?react"
 import LoaderCircle from "./loader.svg?react"
 import MoonIcon from "./moon.svg?react"
 import SunIcon from "./sun.svg?react"
 import TasksIcon from "./tasks.svg?react"
+import Tasks2Icon from "./tasks-2.svg?react"
 import SendTotrash from "./trash.svg?react"
 import TrashIcon from "./trash.svg?react"
 
@@ -19,11 +21,13 @@ export {
   ChevronRightIcon,
   CludSunIcon,
   DetailsIcon,
+  GlassWaterIcon,
   HomeIcon,
   LoaderCircle,
   MoonIcon,
   SendTotrash,
   SunIcon,
+  Tasks2Icon,
   TasksIcon,
   TrashIcon,
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-// import DashboardCards from "../components/DashboardCards"
+import DashboardCards from "../components/DashboardCards"
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar"
 import TaskItem from "../components/TaskItem"
@@ -11,7 +11,7 @@ const HomePage = () => {
       <Sidebar />
       <div className="w-full space-y-6 px-8 py-16">
         <Header subtitle="Dashboard" title="Dashboard" />
-        {/* <DashboardCards /> */}
+        <DashboardCards />
         <div className="grid grid-cols-[1.5fr,1fr] gap-6">
           <div className="space-y-6 rounded-[10px] bg-white p-6">
             <div>
